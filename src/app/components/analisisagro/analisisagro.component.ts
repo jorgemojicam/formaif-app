b5aee8309be36114a7ffaa691cbaa9ae57905c54
Jorge Mojica <jorge.mojica92@gmail.com>
2021-01-13 14:07:41 -0500

    cambios menu y errores en analisis agro

--- src/app/components/analisisagro/analisisagro.component.ts
+++ src/app/components/analisisagro/analisisagro.component.ts
@@ -14,31 +14,33 @@ import DataSelect from '../../data-select/dataselect.json';
 })
 export class AnalisisagroComponent implements OnInit {
 
-  datasolicitud: Solicitud = new Solicitud()
-  tipoAsesor: number;
-  fechahoy:string;
-  
   constructor(
     private activeRoute: ActivatedRoute,
     private sanitizer: DomSanitizer,
-    public srvSol: IdbSolicitudService,) { }
+    public srvSol: IdbSolicitudService
+  ) { }
+
+  datasolicitud: Solicitud = new Solicitud()
+  tipoAsesor: number;
+  fechahoy: string;
+  sol: string;
 
   ngOnInit(): void {
-    this.activeRoute.queryParamMap
-      .subscribe((params) => {
-        let sol = params.get('solicitud')
-        this.srvSol.getSol(sol).subscribe((datasol) => {
-          let hoy = new Date()
-          let mes: number = hoy.getMonth() + 1
-          this.fechahoy = hoy.getDate() + "/" + mes + "/" + hoy.getFullYear()
-          this.datasolicitud = datasol as Solicitud
-          this.tipoAsesor = this.datasolicitud.asesor
-        })
-      });
+    this.activeRoute.queryParamMap.subscribe((params) => {
+      this.sol = params.get('solicitud')
+      this.srvSol.getSol(this.sol).subscribe((datasol) => {
+        let hoy = new Date()
+        let mes: number = hoy.getMonth() + 1
+        this.fechahoy = hoy.getDate() + "/" + mes + "/" + hoy.getFullYear()
+        this.datasolicitud = datasol as Solicitud
+        this.tipoAsesor = this.datasolicitud.asesor
+      })
+    });
+
   }
 
-  transform(base) {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(base);
-  }
+  // transform(base) {
+  //   return this.sanitizer.bypassSecurityTrustResourceUrl(base);
+  // }
 
 }
