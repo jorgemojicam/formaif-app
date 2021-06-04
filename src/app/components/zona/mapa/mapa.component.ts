959a4fd90cd0722a04cb181a29d4c58e00d804ed
Jorge Mojica <jorge.mojica92@gmail.com>
2021-06-04 17:38:56 -0500

    se agrego mapa esri

--- src/app/components/zona/mapa/mapa.component.ts
+++ src/app/components/zona/mapa/mapa.component.ts
@@ -4,10 +4,10 @@ import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import Graphic from "@arcgis/core/Graphic";
+import Draw from "@arcgis/core/views/draw/Draw";
 import Sketch from "@arcgis/core/widgets/Sketch";
+import SketchViewModel from "@arcgis/core/widgets/Sketch";
 esriConfig.apiKey = "AAPK7d8e7f5fbe484587845583d624ad5f4661U73yeiDt-rdiitWwxbj92mqQMGlhYgfJmb97ZZpalTLhniXfHunb8ajs2yB_44";
-
-import { loadModules } from "esri-loader";
 import esri = __esri; // Esri TypeScript Types
 
 @Component({
@@ -21,36 +21,26 @@ export class MapaComponent implements OnInit {
 
   private _zoom = 15;
 
-  private _center: Array<number> = [-73.11551918100278,7.06165317851025];
+  private _center: Array<number> = [-73.11551918100278, 7.06165317851025];
   private _basemap = "topo";
   private _loaded = false;
   private _view: esri.MapView = null;
-  private _poligon
 
   constructor(
 
   ) { }
 
   async initializeMap() {
+    const that = this
     try {
-      // Load the modules for the ArcGIS API for JavaScript
-      /*
-      const [EsriMap, EsriMapView] = await loadModules([
-        "esri/Map",
-        "esri/views/MapView"
-      ]);
-      */
 
       // Configure the Map
       const mapProperties: esri.MapProperties = {
         basemap: this._basemap
       };
-
       const map: esri.Map = new Map(mapProperties);
-
       const graphicsLayer = new GraphicsLayer();
       map.add(graphicsLayer);
-
       // Initialize the MapView
       const mapViewProperties: esri.MapViewProperties = {
         container: this.mapViewEl.nativeElement,
@@ -62,20 +52,50 @@ export class MapaComponent implements OnInit {
       this._view = new MapView(mapViewProperties);
 
       await this._view.when(() => {
-        const sketch = new Sketch({
-          layer: graphicsLayer,
-          view: this._view,
-          // graphic will be selected as soon as it is created
-          creationMode: "update"
-        });
 
-        this._view.ui.add(sketch, "top-right");
-      }); 
+      });
 
-      this._view.on("click", function(evt) {
-        console.log(evt)
-      })
+      let sketchVM = new SketchViewModel({
 
+        layer: graphicsLayer,
+        view: this._view,
+        visibleElements: {
+          createTools: {
+            point: false,
+            circle: false,
+            rectangle: false,
+            polyline: false
+          },
+          selectionTools: {
+            "lasso-selection": false
+          },
+        },
+      });
+
+      sketchVM.create("polygon", { mode: "freehand" });
+      // listen to create event, only respond when event's state changes to complete
+      sketchVM.on("create", function (event) {
+        if (event.state === "complete") {
+          // remove the graphic from the layer associated with the sketch widget
+          // instead use the polygon that user created to query features that
+          // intersect it.
+          console.log(event.graphic.geometry)
+          that.createPolygonGraphic(event.graphic.geometry);
+          //selectFeatures(event.graphic.geometry);
+        }
+      });
+
+      sketchVM.on("update", function (event) {
+        if (event.state === "complete") {
+          // remove the graphic from the layer associated with the sketch widget
+          // instead use the polygon that user created to query features that
+          // intersect it.
+          console.log(event.graphics)          
+          //selectFeastures(event.graphic.geometry);
+        }
+      });
+
+      this._view.ui.add(sketchVM, "top-right");
       return this._view;
     } catch (error) {
       console.log("EsriLoader: ", error);
@@ -83,69 +103,29 @@ export class MapaComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    const that = this
     this.initializeMap().then(mapView => {
-      // The map has been initialized
-      console.log("mapView ready: ", this._view.ready);
-      console.log("puntos: ", this._view.viewpoint.targetGeometry);
-      console.log("mapView ",mapView)
-      this._loaded = this._view.ready;
+      this._loaded = that._view.ready;
     });
-    
+  }
 
+  createPolygonGraphic(polygon) {
+    //this._view.graphics.removeAll();
 
-    /*
-    const map = new Map({
-      basemap: "arcgis-topographic"
-    });
-
-    const graphicsLayer = new GraphicsLayer();
-    map.add(graphicsLayer);
-
-
-    const polygon = {
-      type: "polygon",
-      rings: [
-        [-118.818984489994, 34.0137559967283], //Longitude, latitude
-        [-118.806796597377, 34.0215816298725], //Longitude, latitude
-        [-118.791432890735, 34.0163883241613], //Longitude, latitude
-        [-118.79596686535, 34.008564864635],   //Longitude, latitude
-        [-118.808558110679, 34.0035027131376]  //Longitude, latitude
-      ]
-    };
     const simpleFillSymbol = {
       type: "simple-fill",
       color: [227, 139, 79, 0.8],  // Orange, opacity 80%
       outline: {
-        color: [255, 255, 255],
+        color: "purple",
         width: 1
       }
     };
-    const polygonGraphic = new Graphic({
+    let graphic = new Graphic({
       geometry: polygon,
       symbol: simpleFillSymbol,
     });
 
-    graphicsLayer.add(polygonGraphic);
-
-    const view = new MapView({
-      container: "viewDiv",
-      map: map,
-      center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
-    });
-
-    view.when(() => {
-      const sketch = new Sketch({
-        layer: graphicsLayer,
-        view: view,
-        // graphic will be selected as soon as it is created
-        creationMode: "update"
-      });
-
-      view.ui.add(sketch, "top-right");
-    });
-*/
+    this._view.graphics.add(graphic);
   }
 
   ngOnDestroy() {
